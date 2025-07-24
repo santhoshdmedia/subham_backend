10f33e7586f710eae20f8980b19f66b31f8e391b
santhoshdmedia <devconsoledmedia@gmail.com>
2025-07-24 18:56:10 +0530

    changed limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,8 +10,6 @@ const Inquiry = require('../models/email');
 
 // Rate limiting configuration
 const otpLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // Limit each IP to 3 OTP requests per windowMs
   message: 'Too many OTP requests from this IP, please try again later',
   standardHeaders: true,
   legacyHeaders: false
